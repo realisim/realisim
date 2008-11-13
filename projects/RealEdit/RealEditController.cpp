37fd9c801f54c8f3d8e6736de5aa6c2e84f96c3e
pierreolivier.beaudoin <pierreolivier.beaudoin@73781e26-a14f-11dd-a887-7ffe1a420f8d>
2008-11-13 13:39:56 +0000

    Simplification.... RealEdit ne servira qu'a faire l'edition 3d. Donc on delete le ui d'animation ainsi que la super classe Workspace Ui. Maintenant, l'application instancie une fenêtre d'édition qui elle instancie son controller et voila!

--- projects/RealEdit/RealEditController.cpp
+++ projects/RealEdit/RealEditController.cpp
@@ -1,68 +1,12 @@
 
 #include "RealEditController.h"
 
-#include "AnimationUi.h"
 #include "EditionUi.h"
-
-#include <QDockWidget>
-#include <QFrame>
-#include <QMainWindow>
-#include <QMenuBar>
-#include <QTabWidget>
-#include <QStatusBar>
-#include <QVBoxLayout>
 
 using namespace RealEdit;
 
 RealEditController::RealEditController()
-: QObject()
-,mpMainWindow( 0 )
-, mpEditionUi( 0 )
-, mpAnimationUi( 0 )
 {
-	mpMainWindow = new QMainWindow();
-	mpMainWindow->show();
-	mpMainWindow->setAnimated( true );
-	mpMainWindow->setMinimumSize( 800, 600 );
-
-    //Create the central widget
-	{
-		QTabWidget* pCentralWidget = new QTabWidget( mpMainWindow );
-		pCentralWidget->setTabPosition( QTabWidget::South );
-		//set the central widget of the main window
-		mpMainWindow->setCentralWidget( pCentralWidget );    
-
-		QVBoxLayout* pMainVLyt = new QVBoxLayout( pCentralWidget );
-
-		QObject::connect( pCentralWidget, SIGNAL( currentChanged ( int ) ),
-						  this, SLOT( handleCurrentTabChanged( int ) ) );
-
-		//Add the edition page
-		addEditionUi( pCentralWidget );
-
-		//Add the animation page
-		addAnimationUi( pCentralWidget );
-	}
-
-    //create a menu bar
-    QMenuBar* pMenuBar = new QMenuBar();
-
-    //add item to the menu bar and to the catalog
-	pMenuBar->addMenu( QObject::tr( "&File" ) );
-    pMenuBar->addMenu( QObject::tr( "&Edit" ) );
-    pMenuBar->addMenu( QObject::tr( "&Tools" ) );
-
-    //add a menu to the mainWindow
-    mpMainWindow->setMenuBar( pMenuBar );
-
-    //add a tool bar
-
-    //add a statusBar
-    QStatusBar* statusBar = new QStatusBar( mpMainWindow );
-    mpMainWindow->setStatusBar( statusBar );
-
-	hideAllWorkspaceUiWidgets();
-	showWorkspaceUiWidgets( mpEditionUi );
 }
 
 RealEditController::~RealEditController()
@@ -70,81 +14,9 @@ RealEditController::~RealEditController()
 }
 
 void
-RealEditController::addEditionUi( QTabWidget* ipCentralWidget )
+RealEditController::newProject()
 {
-	//Add the edition page
-	mpEditionUi = new EditionUi( ipCentralWidget, *this );
-	ipCentralWidget->insertTab( EditionTabIndex, mpEditionUi, QObject::tr( "Edition" ) );
+	new EditionUi();
 }
 
-void
-RealEditController::addAnimationUi( QTabWidget* ipCentralWidget )
-{
-	//Add the animation page
-	mpAnimationUi = new AnimationUi( ipCentralWidget, *this );
-	ipCentralWidget->insertTab( AnimationTabIndex, mpAnimationUi, QObject::tr( "Animation" ) );
-}
 
-QDockWidget*
-RealEditController::addDockWidget( WorkspaceUi* ipRequester )
-{
-	QDockWidget* pDockWidget = new QDockWidget( mpMainWindow );
-	mpMainWindow->addDockWidget( Qt::LeftDockWidgetArea, pDockWidget );
-	
-	mWorkspaceUiToWidgetMap.insert( std::make_pair( ipRequester, pDockWidget ) );
-
-	return pDockWidget;
-}
-
-void
-RealEditController::hideAllWorkspaceUiWidgets()
-{
-	std::map<WorkspaceUi*, QWidget*>::iterator it = mWorkspaceUiToWidgetMap.begin();
-	while( it != mWorkspaceUiToWidgetMap.end() )
-	{
-		it->second->hide();
-		it++;
-	}
-}
-
-void
-RealEditController::showWorkspaceUiWidgets( WorkspaceUi* ipWorkspace )
-{
-	std::pair<WorkspaceUiToWidgetMap::iterator, WorkspaceUiToWidgetMap::iterator> range =
-		mWorkspaceUiToWidgetMap.equal_range( ipWorkspace );
-
-	while( range.first != range.second )
-	{
-		range.first->second->show();
-		range.first++;
-	}
-}
-
-void
-RealEditController::hideWorkspaceUiWidgets( WorkspaceUi* ipWorkspace )
-{
-		std::pair<WorkspaceUiToWidgetMap::iterator, WorkspaceUiToWidgetMap::iterator> range =
-		mWorkspaceUiToWidgetMap.equal_range( ipWorkspace );
-
-	while( range.first != range.second )
-	{
-		range.first->second->hide();
-		range.first++;
-	}
-}
-
-void
-RealEditController::handleCurrentTabChanged( int iIndex )
-{
-	switch( iIndex )
-	{
-	case EditionTabIndex:
-		hideAllWorkspaceUiWidgets();
-		showWorkspaceUiWidgets( mpEditionUi );
-		break;
-	case AnimationTabIndex:
-		hideAllWorkspaceUiWidgets();
-		showWorkspaceUiWidgets( mpAnimationUi );
-		break;
-	}
-}
--- projects/RealEdit/RealEditController.h
+++ projects/RealEdit/RealEditController.h
@@ -2,19 +2,6 @@
 RealEditController.h
 
 
-RealEditController gère la creation de tout le UI de l'application. Il permet
-aussi au diffent WorspaceUi ( animation, edition ... ) d'ajouter des éléments
-( QDockWiget, items de menu etc... ) a la MainWindow. Il gère aussi le 
-hide/show des widgets propre a un workspaceUi. Par exemple, le ui d'édition
-a besoin d'un dockwidget qui contient le navigateur d'object. Par contre, 
-le ui d'animation n'a pas besoin de ce dockwidget. Quand l'usager cliquera
-sur le tab de l'animation, le dockwidget d'édition ( ainsi que tout les autres
-widgets propre à l'édition ) se cacheront et les widgets d'animation 
-s'afficheront.
-
-important: Il faut que tout le ui soit créé par les
-constructeurs des workspaceUi ( edition, animation, etc... ) pour que 
-RealEditContoller puisse initialiser correctement le Ui de l'application.
 
 -----------------------------------------------------------------------------*/
 
@@ -22,59 +9,21 @@ RealEditContoller puisse initialiser correctement le Ui de l'application.
 #ifndef RealEdit_RealEditController_hh
 #define RealEdit_RealEditController_hh
 
-#include <map>
-
-#include <QObject>
-
-class QDockWidget;
-class QTabWidget;
-class QMainWindow;
-class QWidget;
 
 namespace RealEdit{ class RealEditController;
-					class EditionUi;
-					class AnimationUi;
-					class WorkspaceUi; }
+					class EditionUi; }
 
-class RealEdit::RealEditController : public QObject
+class RealEdit::RealEditController 
 {
-	Q_OBJECT
 public:
 	RealEditController();
 	~RealEditController();
-
-	enum WorkspaceUiTabIndex
-	{
-		EditionTabIndex = 0,
-		AnimationTabIndex
-	};
-
-	//----------------gestion du ui
-	QDockWidget* addDockWidget( WorkspaceUi* ipRequester );
-	//QMenuItem* addMenuItem( a venir... );
+	
+	void newProject();
 
 protected:
 
-private slots:
-	void handleCurrentTabChanged( int );
-
 private:
-	void addEditionUi( QTabWidget* ipCentralWidget );
-	void addAnimationUi( QTabWidget* ipCentralWidget );
-
-	void hideAllWorkspaceUiWidgets();
-	void showWorkspaceUiWidgets( WorkspaceUi* ipWorkspace );
-	void hideWorkspaceUiWidgets( WorkspaceUi* ipWorkspace );
-
-	QMainWindow* mpMainWindow;
-	EditionUi* mpEditionUi;
-	AnimationUi* mpAnimationUi;
-
-	//cette map contient la relation entre le WorkspaceUi et tous ses
-	//widgets qui sont rattachés a la MainWindow ( le dockWidget, les
-	//items de menu, items de tool bar etc... )
-	typedef std::map<WorkspaceUi*, QWidget*> WorkspaceUiToWidgetMap;
-	WorkspaceUiToWidgetMap mWorkspaceUiToWidgetMap;
 };
 
 #endif //RealEdit_RealEditController_hh
