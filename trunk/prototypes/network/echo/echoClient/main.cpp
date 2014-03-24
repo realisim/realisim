52b4dcef3f97ed1b8c6d727fa6fa940cd681b158
pierreolivier.beaudoin <pierreolivier.beaudoin@73781e26-a14f-11dd-a887-7ffe1a420f8d>
2014-03-24 12:46:10 +0000

    Ajout d'un serveur/client écho afin de debogger les classes client/server

---
+++ trunk/prototypes/network/echo/echoClient/main.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+#include <QApplication>
+#include <QIcon>
+#include <QMainWindow>
+#include "Widget.h"
+
+
+int startMainApp()
+{
+    if ( qApp )
+    {
+        return qApp->exec();
+    }
+    else
+    {
+        printf("ERROR: unable to start Pong\n");
+        return 0;
+    }
+}
+
+
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
+    QTextCodec::setCodecForCStrings (QTextCodec::codecForName("UTF-8"));
+    QMainWindow mw;
+    realisim::prototypes::Widget w(&mw);
+    mw.setCentralWidget(&w);
+    
+    mw.move(10, 40);
+    mw.resize(480, 320);
+    mw.show();
+    	
+    if ( startMainApp() == 0 )
+    {
+        //we are closing!
+    }
+    return 0;
+}
