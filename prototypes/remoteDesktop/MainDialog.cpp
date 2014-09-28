a6981c39c16e2259bd50b3bd1bcc0c65ddb149b3
pierreolivier.beaudoin <pierreolivier.beaudoin@73781e26-a14f-11dd-a887-7ffe1a420f8d>
2014-09-28 13:33:36 +0000

    - Les infos du remoteDesktop sont passées lors de la connexion client. - Les événements de souris (déplacement et bouton gauche) sont correctement gérés.

--- prototypes/remoteDesktop/MainDialog.cpp
+++ prototypes/remoteDesktop/MainDialog.cpp
@@ -1,16 +1,64 @@
 
 #include "MainDialog.h"
+#ifdef _WIN32
+	#include <windows.h>
+#endif
 
+using namespace std;
 using namespace realisim;
 
 //-----------------------------------------------------------------------------
 //--- protocol
 //-----------------------------------------------------------------------------  
-int MainDialog::protocol::mVersion = 1;
-MainDialog::protocol::protocol()
+int protocol::mVersion = 1;
+protocol::protocol()
 {}
 //-----------------------------------------------------------------------------
-QByteArray MainDialog::protocol::makeMessageNewFrame( QPixmap iP )
+QByteArray protocol::makeMessageGiveDesktopInfo( QByteArray iBa )
+{
+	QByteArray r;
+  QDataStream ds( &r, QIODevice::WriteOnly );
+  ds.setVersion(QDataStream::Qt_4_7);
+  ds << mVersion;
+  ds << (qint32)mGiveDesktopInfo;
+  ds << iBa;
+  return r;
+}
+//-----------------------------------------------------------------------------
+QByteArray protocol::makeMessageMouseMoved( QPointF iP )
+{
+  QByteArray r;
+  QDataStream ds( &r, QIODevice::WriteOnly );
+  ds.setVersion(QDataStream::Qt_4_7);
+  ds << mVersion;
+  ds << (qint32)mMouseMoved;
+  ds << iP;
+  return r;
+}
+//-----------------------------------------------------------------------------
+QByteArray protocol::makeMessageMousePressed( QPointF iP )
+{
+  QByteArray r;
+  QDataStream ds( &r, QIODevice::WriteOnly );
+  ds.setVersion(QDataStream::Qt_4_7);
+  ds << mVersion;
+  ds << (qint32)mMousePressed;
+  ds << iP;
+  return r;
+}
+//-----------------------------------------------------------------------------
+QByteArray protocol::makeMessageMouseReleased( QPointF iP )
+{
+  QByteArray r;
+  QDataStream ds( &r, QIODevice::WriteOnly );
+  ds.setVersion(QDataStream::Qt_4_7);
+  ds << mVersion;
+  ds << (qint32)mMouseReleased;
+  ds << iP;
+  return r;
+}
+//-----------------------------------------------------------------------------
+QByteArray protocol::makeMessageNewFrame( QPixmap iP )
 {
 	QByteArray r;
   QDataStream ds( &r, QIODevice::WriteOnly );
@@ -21,23 +69,132 @@ QByteArray MainDialog::protocol::makeMessageNewFrame( QPixmap iP )
   return r;
 }
 //-----------------------------------------------------------------------------
-QByteArray MainDialog::protocol::makeMessageGiveMaster()
+QByteArray protocol::makeMessageRequestDesktopInfo()
 {
 	QByteArray r;
   QDataStream ds( &r, QIODevice::WriteOnly );
   ds.setVersion(QDataStream::Qt_4_7);
   ds << mVersion;
-  ds << (qint32)mGiveMaster;
+  ds << (qint32)mRequestDesktopInfo;
   return r;
 }
+//-----------------------------------------------------------------------------
+QString protocol::toString( message m )
+{
+	QString r( "unknown message" );
+  switch (m) 
+  {
+    case mRequestDesktopInfo: r = "mRequestDesktopInfo"; break;
+    case mGiveDesktopInfo: r = "mGiveDesktopInfo"; break;
+    case mNewFrame: r = "mNewFrame"; break;
+    case mMouseMoved: r = "mMouseMoved"; break;
+    case mMousePressed: r = "mMousePressed"; break;
+    case mMouseReleased: r = "mMouseReleased"; break;
+    default: break;
+  }
+  return r;
+}
+//-----------------------------------------------------------------------------
+//--- desktopInfo
+//-----------------------------------------------------------------------------  
+int desktopInfo::mVersion = 1;
 
+desktopInfo::desktopInfo() :
+  mScreenSizes()
+{}
+desktopInfo::~desktopInfo()
+{ clear(); }
+//-----------------------------------------------------------------------------
+void desktopInfo::clear()
+{
+	mScreenSizes.clear();
+}
+//-----------------------------------------------------------------------------
+int desktopInfo::getNumberOfScreens() const
+{ return mScreenSizes.size(); }
+//-----------------------------------------------------------------------------
+QSize desktopInfo::getScreenSize( int i ) const
+{
+	QSize r;
+  if( i >= 0 && i <= getNumberOfScreens() - 1 ) 
+  { r = mScreenSizes[i]; }
+  return r;
+}
+//-----------------------------------------------------------------------------
+void desktopInfo::fromBinary( QByteArray iBa )
+{
+	qint32 version;
+  qint32 numberOfScreens;
+  QSize s;
+  QDataStream ds( &iBa, QIODevice::ReadOnly );
+  ds.setVersion(QDataStream::Qt_4_7);
+  ds >> version;
+  ds >> numberOfScreens;
+  setNumberOfScreens( numberOfScreens );
+  for( int i = 0; i < getNumberOfScreens(); ++i )
+  {
+  	ds >> s;
+    setScreenSize( i, s );
+  }
+}
+//-----------------------------------------------------------------------------
+QByteArray desktopInfo::toBinary() const
+{
+	QByteArray r;
+  QDataStream ds( &r, QIODevice::WriteOnly );
+  ds.setVersion(QDataStream::Qt_4_7);
+  ds << (qint32)mVersion;
+  ds << (qint32)getNumberOfScreens();
+  for( int i = 0; i < getNumberOfScreens(); ++i )
+  { ds << getScreenSize( i ); }
+  return r;
+}
+//-----------------------------------------------------------------------------
+void desktopInfo::setNumberOfScreens( int i )
+{
+	mScreenSizes.clear();
+  mScreenSizes.resize( i );
+}
+//-----------------------------------------------------------------------------
+void desktopInfo::setScreenSize( int i, QSize iS )
+{ mScreenSizes[i] = iS; }
+
+//-----------------------------------------------------------------------------
+//--- graphicsView
+//-----------------------------------------------------------------------------  
+graphicsView::graphicsView( QGraphicsScene* ipScene, QWidget* ipParent/*= 0*/ ) :
+	QGraphicsView( ipScene, ipParent )
+{}
+
+graphicsView::~graphicsView()
+{}
+//-----------------------------------------------------------------------------
+void graphicsView::keyPressEvent( QKeyEvent* ipE )
+{}
+//-----------------------------------------------------------------------------
+void graphicsView::keyReleaseEvent( QKeyEvent* ipE )
+{}
+//-----------------------------------------------------------------------------
+void graphicsView::mouseDoubleClickEvent( QMouseEvent* ipE )
+{}
+//-----------------------------------------------------------------------------
+void graphicsView::mouseMoveEvent( QMouseEvent* ipE )
+{ emit mouseMoved( ipE->pos() ); }
+//-----------------------------------------------------------------------------
+void graphicsView::mousePressEvent( QMouseEvent* ipE )
+{ emit mousePressed( ipE->pos() ); }
+//-----------------------------------------------------------------------------
+void graphicsView::mouseReleaseEvent( QMouseEvent* ipE )
+{ emit mouseReleased( ipE->pos() ); }
 //-----------------------------------------------------------------------------
 //--- MainDialog
 //-----------------------------------------------------------------------------  
 MainDialog::MainDialog() : QMainWindow(),
   mClientMode( cmIdle ),
   mServerMode( smIdle ),
-  mServerTimerId(0)
+  mServerTimerId(0),
+  mClientLog(),
+  mServerLog()
 {
 	createUi();
   
@@ -55,11 +212,32 @@ MainDialog::MainDialog() : QMainWindow(),
   	this, SLOT( serverSocketConnected( int ) ) );
   connect( &mServer, SIGNAL( socketDisconnected( int ) ),
   	this, SLOT( serverSocketDisconnected( int ) ) );
+  connect( &mServer, SIGNAL( downloadEnded( int, int ) ),
+  	this, SLOT( serverDownloadEnded( int, int ) ) );
+
   
   mServer.startServer( 12345 );
   
+  mClientLog.logTimestamp(true);
+  mClientLog.logToFile( true, "clientLog.txt" );
+  mServerLog.logTimestamp(true);
+  mServerLog.logToFile( true, "serverLog.txt" );
+  
+  
+  //fill desktop info
+  QDesktopWidget* dw = QApplication::desktop();
+  mDesktopInfo.setNumberOfScreens( dw->screenCount() );
+  for( int i = 0; i < mDesktopInfo.getNumberOfScreens(); ++i )
+  { mDesktopInfo.setScreenSize( i, dw->screenGeometry( i ).size() ); }
   updateUi();
 }
+
+MainDialog::~MainDialog()
+{
+	mClient.disconnect();
+	mServer.stopServer();
+}
+
 //-----------------------------------------------------------------------------
 void MainDialog::clientDownloadEnded( int iId )
 {
@@ -69,25 +247,35 @@ void MainDialog::clientDownloadEnded( int iId )
 //-----------------------------------------------------------------------------
 void MainDialog::clientSocketConnected()
 {
+//showFullScreen();
+//mpView->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+//mpView->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+	mClient.send( protocol::makeMessageRequestDesktopInfo() );
+	
 	setClientMode( cmActiveSlave );
   updateUi();
+  
+  mClientLog.log( "client connected." );
 }
 //-----------------------------------------------------------------------------
 void MainDialog::clientSocketDisconnected()
-{
+{	
 	setClientMode( cmIdle );
   updateUi();
+  
+  mClientLog.log( "client disconnected." );
 }
 //-----------------------------------------------------------------------------
 void MainDialog::createUi()
 {
-  resize(800, 600);
+  //resize(800, 600);
   
   QFrame* pMainFrame = new QFrame(this);
   setCentralWidget(pMainFrame);
   
   QVBoxLayout* pLyt = new QVBoxLayout(pMainFrame);
-  pLyt->setMargin(2);
+  pLyt->setMargin(0);
   pLyt->setSpacing(5);
   
   //--- Client - frame de connection
@@ -132,15 +320,26 @@ void MainDialog::createUi()
   //--- Client - frame d'activité
   mpClientActivityFrame = new QFrame( pMainFrame );
   QVBoxLayout* pClientActivityLyt = new QVBoxLayout( mpClientActivityFrame );
-  pClientActivityLyt->setMargin(2);
-  pClientActivityLyt->setSpacing(2);
+  pClientActivityLyt->setMargin(0);
+  pClientActivityLyt->setSpacing(0);
   {
-  	mpLabel = new QLabel( mpClientActivityFrame );
-    pClientActivityLyt->addWidget( mpLabel );
+  	//mpLabel = new QLabel( mpClientActivityFrame );
+    mpScene = new QGraphicsScene( mpClientActivityFrame );
+    mpView = new graphicsView( mpScene, mpClientActivityFrame );
+    
+    connect( mpView, SIGNAL( mouseMoved( QPoint ) ),
+    	this, SLOT( mouseMovedInView(QPoint) ) );
+    connect( mpView, SIGNAL( mousePressed( QPoint ) ),
+    	this, SLOT( mousePressedInView( QPoint ) ) );
+    connect( mpView, SIGNAL( mouseReleased( QPoint ) ),
+    	this, SLOT( mouseReleasedInView( QPoint ) ) );
+    
+    pClientActivityLyt->addWidget( mpView );
   }
   
   //--- Server - frame d'activité 
   mpServerFrame = new QFrame( pMainFrame );
+  mpServerFrame->setMaximumHeight( 30 );
   QVBoxLayout* pServerLyt = new QVBoxLayout( mpServerFrame );
   pServerLyt->setMargin( 2 );
   pServerLyt->setSpacing( 2 );
@@ -149,7 +348,6 @@ void MainDialog::createUi()
     pServerLyt->addStretch( 1 );
     pServerLyt->addWidget( mpServerInfo );
   }
-  
   
   //ajoute le left panel au layout principale
   pLyt->addWidget( mpClientConnectionFrame );
@@ -161,6 +359,46 @@ void MainDialog::connectClicked()
 {
 	mClient.disconnect();
   mClient.connectToTcpServer(mpHostName->text(), mpHostPort->text().toInt());
+}
+//-----------------------------------------------------------------------------
+void MainDialog::handleMessageFromClient( int iSocket, QByteArray iBa )
+{
+	qint32 version;
+  qint32 m;
+	QDataStream ds( &iBa, QIODevice::ReadOnly );
+  ds.setVersion( QDataStream::Qt_4_7 );
+  ds >> version;
+  if( version == protocol::mVersion )
+  {
+  	ds >> m;
+    switch ( (protocol::message)m ) 
+    {
+    	case protocol::mMouseMoved:
+    	case protocol::mMousePressed:
+      case protocol::mMouseReleased:
+      {
+      	QPointF p;
+        ds >> p;
+        handleMouseInputFromClient( (protocol::message)m, p );
+      }
+      break;
+    	case protocol::mRequestDesktopInfo:
+      {
+      	QByteArray ba = protocol::makeMessageGiveDesktopInfo( 
+        	mDesktopInfo.toBinary() );
+        mServer.send( iSocket, ba );
+      }
+      break;
+      default: break;
+    }
+    mServerLog.log( "handleMessageFromClient: %s", 
+    	protocol::toString((protocol::message)m).toStdString().c_str() );
+  }
+  else {
+    //pas la bonne version du protocole...
+    mServerLog.log( "handleMessageFromClient: wrong protocol version.\
+ expected %d, received %d", protocol::mVersion, version );
+  }
 }
 //-----------------------------------------------------------------------------
 void MainDialog::handleMessageFromServer( QByteArray iBa )
@@ -175,36 +413,114 @@ void MainDialog::handleMessageFromServer( QByteArray iBa )
   	ds >> m;
     switch ( (protocol::message)m ) 
     {
-      case protocol::mGiveMaster : break;
+    	case protocol::mGiveDesktopInfo:
+      {
+      	QByteArray ba;
+        ds >> ba;
+        mRemoteDesktopInfo.fromBinary( ba );
+mClientLog.log( "Remote desktop size: %dx%d", 
+	mRemoteDesktopInfo.getScreenSize(0).width(), 
+  mRemoteDesktopInfo.getScreenSize(0).height() );
+      }
+      break;
       case protocol::mNewFrame : 
       {
       	ds >> mDesktopPixmap;
-        mDesktopPixmap.save( "a.png", "PNG" );
         updateUi();
       }
       break;
       default: break;
     }
+    mClientLog.log( "handleMessageFromServer: %s", 
+    	protocol::toString((protocol::message)m).toStdString().c_str() );
   }
   else {
+  	mClientLog.log( "handleMessageFromServer: wrong protocol version.\
+ expected %d, received %d", protocol::mVersion, version );
     //pas la bonne version du protocole...
   }
+}
+//-----------------------------------------------------------------------------
+void MainDialog::handleMouseInputFromClient( protocol::message iM, QPointF iP )
+{
+	mServerLog.log( "handleMouseInputFromClient %d -> %.2f, %.2f", iM, 
+  	iP.x(), iP.y() );
+    
+#ifdef _WIN32
+  INPUT input;
+  input.mi.mouseData = 0; 
+  input.mi.dx =  iP.x()*( 65535/(double)mDesktopInfo.getScreenSize(0).width() );//x being coord in pixels
+  input.mi.dy =  iP.y()*( 65535/(double)mDesktopInfo.getScreenSize(0).height() );//y being coord in pixels
+  input.type = INPUT_MOUSE;
+  input.mi.time = 0;
+  input.mi.dwExtraInfo = 0;
 
+  switch (iM) 
+  {
+  	case protocol::mMouseMoved:
+    	input.mi.dwFlags = (MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE );
+    break;
+    case protocol::mMousePressed:
+      input.mi.dwFlags = (MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE | MOUSEEVENTF_LEFTDOWN);
+    break;
+    case protocol::mMouseReleased:
+      input.mi.dwFlags = (MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE | MOUSEEVENTF_LEFTUP);
+    break;
+    default: break;
+  }
+  SendInput( 1, &input, sizeof(input) );
+  //mServerLog.log( " mi.dx: %d, mi.dy: %d SendInput returns: %d, GetLastError: %d", 
+  //   input.mi.dx, input.mi.dy , a, GetLastError() );
+#endif
+}
+//-----------------------------------------------------------------------------
+void MainDialog::mouseMovedInView( QPoint iP )
+{
+	mClientLog.log( "mouse moved at pos %d, %d -> %.2f, %.2f",
+  	iP.x(), iP.y(),
+    mpView->mapToScene( iP ).x(),
+    mpView->mapToScene( iP ).y() );
+  mClient.send( protocol::makeMessageMouseMoved( mpView->mapToScene( iP ) ) );
+}
+//-----------------------------------------------------------------------------
+void MainDialog::mousePressedInView( QPoint iP )
+{
+	mClientLog.log( "mouse pressed at pos %d, %d -> %.2f, %.2f",
+  	iP.x(), iP.y(),
+    mpView->mapToScene( iP ).x(),
+    mpView->mapToScene( iP ).y() );
+  mClient.send( protocol::makeMessageMousePressed( mpView->mapToScene( iP ) ) );
+}
+//-----------------------------------------------------------------------------
+void MainDialog::mouseReleasedInView( QPoint iP )
+{
+	mClientLog.log( "mouse released at pos %d, %d -> %.2f, %.2f",
+  	iP.x(), iP.y(),
+    mpView->mapToScene( iP ).x(),
+    mpView->mapToScene( iP ).y() );
+  mClient.send( protocol::makeMessageMouseReleased( mpView->mapToScene( iP ) ) );
+}
+//-----------------------------------------------------------------------------
+void MainDialog::serverDownloadEnded( int iSocket, int iId )
+{
+	QByteArray ba = mServer.getDownload( iSocket, iId );
+  handleMessageFromClient( iSocket, ba );
 }
 //-----------------------------------------------------------------------------
 void MainDialog::serverSocketConnected( int iIndex )
 {
+	mServerLog.log( "Client %d with ip %s has connected.", iIndex, 
+		mServer.getSocketPeerAddress( iIndex ).toStdString().c_str() );
+    
 	setServerMode( smActive );
-  if( mServer.getNumberOfSockets() == 1 )
-  {
-    QByteArray ba = protocol::makeMessageGiveMaster();
-    mServer.send( iIndex, ba );
-  }
 	updateUi();
 }
 //-----------------------------------------------------------------------------
 void MainDialog::serverSocketDisconnected( int iIndex )
 {
+	mServerLog.log( "Client %d with ip %s has disconnected.", iIndex, 
+		mServer.getSocketPeerAddress( iIndex ).toStdString().c_str() );
+    
 	if( mServer.getNumberOfSockets() == 0 )
   { setServerMode( smIdle ); }
 	updateUi();
@@ -214,6 +530,8 @@ void MainDialog::setClientMode( clientMode iMode )
 {
 	if( iMode != getClientMode() )
   {
+  	mClientLog.log( "Client about to change mode from %d to %d", 
+    	getClientMode(), iMode );
   	switch (iMode)
     {
       case cmIdle: mClientMode = iMode; break;
@@ -228,6 +546,8 @@ void MainDialog::setServerMode( serverMode iMode )
 {
 	if( iMode != getServerMode() )
   {
+  	mServerLog.log( "Server about to change mode from %d to %d", 
+    	getServerMode(), iMode );
   	switch (iMode)
     {
       case smIdle: 
@@ -266,8 +586,9 @@ void MainDialog::updateUi()
     case cmActiveSlave:
     	mpClientConnectionFrame->hide();
       mpClientActivityFrame->show();
-      mpLabel->clear();
-      mpLabel->setPixmap( mDesktopPixmap );
+
+      mpScene->clear();
+      mpScene->addPixmap( mDesktopPixmap );
     	break;
     default: break;
   }
--- prototypes/remoteDesktop/MainDialog.h
+++ prototypes/remoteDesktop/MainDialog.h
@@ -7,22 +7,86 @@
 #include <network/Client.h>
 #include <network/Server.h>
 #include <QtGui>
+#include "utils/log.h"
+#include <vector>
 
 namespace realisim 
 {
 
+class protocol
+{
+public:
+  protocol();
+  enum message{ mRequestDesktopInfo, mGiveDesktopInfo, mMouseMoved,
+    mMousePressed, mMouseReleased, mNewFrame };
+                    
+  static QByteArray makeMessageGiveDesktopInfo( QByteArray );
+  static QByteArray makeMessageMouseMoved( QPointF );
+  static QByteArray makeMessageMousePressed( QPointF );
+  static QByteArray makeMessageMouseReleased( QPointF );
+  static QByteArray makeMessageNewFrame( QPixmap );
+  static QByteArray makeMessageRequestDesktopInfo();
+  static QString toString( message );
+  static int mVersion;
+};
+
+class desktopInfo
+{
+public:
+	desktopInfo();
+  ~desktopInfo();
+
+	void clear();
+	int getNumberOfScreens() const;
+  QSize getScreenSize( int ) const;
+	void fromBinary( QByteArray );
+  QByteArray toBinary() const;
+  void setNumberOfScreens( int );
+  void setScreenSize( int, QSize );
+      
+protected:
+	static int mVersion;
+  std::vector<QSize> mScreenSizes;
+};
+
+class graphicsView : public QGraphicsView
+{
+	Q_OBJECT
+public:	
+	graphicsView( QGraphicsScene*, QWidget* = 0 );
+  virtual ~graphicsView();
+  
+signals:
+	void mouseMoved( QPoint );
+	void mousePressed( QPoint );
+  void mouseReleased( QPoint );
+  
+protected:
+	virtual void keyPressEvent( QKeyEvent* );
+	virtual void keyReleaseEvent( QKeyEvent* );
+	virtual void mouseDoubleClickEvent( QMouseEvent* );
+	virtual void mouseMoveEvent( QMouseEvent* );
+	virtual void mousePressEvent( QMouseEvent* );
+	virtual void mouseReleaseEvent( QMouseEvent* );
+};
+  
+//-----------------------------------------------------------------------------
 class MainDialog : public QMainWindow
 {
 	Q_OBJECT
 public:
 	MainDialog();
-	~MainDialog(){};
+	~MainDialog();
   
 protected slots:
 	void clientDownloadEnded(int);
   void clientSocketConnected();
   void clientSocketDisconnected();
 	void connectClicked();
+  void mouseMovedInView( QPoint );
+  void mousePressedInView( QPoint );
+  void mouseReleasedInView( QPoint );
+  void serverDownloadEnded( int, int );
   void serverSocketConnected( int );
 	void serverSocketDisconnected( int );
 	void updateUi();
@@ -30,22 +94,14 @@ protected slots:
 protected:
 	enum clientMode { cmIdle, cmActiveMaster, cmActiveSlave };
 	enum serverMode { smIdle, smActive };
-  class protocol
-  {
-  	public:
-    	protocol();
-    	enum message{ mRequestMaster, mGiveUpMaster, mGiveMaster, mTakeBackMaster,
-      	mNewFrame };
-        
-      static QByteArray makeMessageGiveMaster();
-      static QByteArray makeMessageNewFrame( QPixmap );
-    	static int mVersion;
-  };
+	
 
 	void createUi();
   clientMode getClientMode() const { return mClientMode; }
   serverMode getServerMode() const {return mServerMode; }
+  void handleMessageFromClient( int, QByteArray );
   void handleMessageFromServer( QByteArray );
+  void handleMouseInputFromClient( protocol::message, QPointF );
   void setClientMode( clientMode );
   void setServerMode( serverMode );
   void timerEvent( QTimerEvent* );
@@ -54,12 +110,19 @@ protected:
   serverMode mServerMode;
   reusables::network::Client mClient;
   reusables::network::Server mServer;
+  int mServerTimerId;
+  utils::Log mClientLog;
+  utils::Log mServerLog;
+  desktopInfo mDesktopInfo;
+  desktopInfo mRemoteDesktopInfo;
   
   //--- pour le client
   QFrame* mpClientConnectionFrame;
   QFrame* mpClientActivityFrame;
   QLineEdit* mpHostName;
   QLineEdit* mpHostPort;
+  graphicsView* mpView;
+  QGraphicsScene* mpScene;
   QLabel* mpLabel;
   
   QPixmap mDesktopPixmap;
@@ -67,8 +130,6 @@ protected:
   //--- pour le server
   QFrame* mpServerFrame;
   QLabel* mpServerInfo;
-  
-  int mServerTimerId;
 };
 
 }
