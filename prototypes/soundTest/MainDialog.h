5c1c3686ccdd7d5b009e258c21fa627edaac56e0
pierreolivier.beaudoin <pierreolivier.beaudoin@73781e26-a14f-11dd-a887-7ffe1a420f8d>
2015-02-15 01:32:10 +0000

    Ajout du prototype soundTest pour la composante sound.

---
+++ prototypes/soundTest/MainDialog.h
@@ -0,0 +1,37 @@
+/*
+ *  MainWindow.h
+ */
+
+#ifndef MainDialog_hh
+#define MainDialog_hh
+
+#include <map>
+#include <QMainWindow>
+#include "sound/AudioInterface.h"
+
+//------------------------------------------------------------------------------
+class MainDialog : public QMainWindow
+{
+	Q_OBJECT
+public:
+	MainDialog();
+	~MainDialog(){};
+
+	enum notes{ nC, nCSharp, nD, nEFlat, nE, nF, nFSharp, nG, nGSharp, nA, nBFlat, 
+  	nB };
+  
+protected slots:
+	void noteClicked( int );
+
+protected:
+	void createUi();
+  void generateNotes();
+  QString toString( notes ) const;
+  void updateUi();
+  
+  realisim::sound::AudioInterface mAudio;
+  int mSourceId;
+  std::map< notes, int > mNotesToBuffer;
+};
+
+#endif
