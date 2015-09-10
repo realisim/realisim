8651cd91bdf2f9ee5d300d48e6e72bec1197fadf
Pierre-Olivier Beaudoin <pierreolivier.beaudoin@gmail.com>
2015-09-09 20:35:15 -0400

    Script de build mac prend la version en parametre - La version est concatené a la string sargam pour faire le .dmg. Par exemple si le parametre est "v.5.1", alors le .dmg sera "Sargam v0.5.1.dmg"

---
+++ prototypes/exercises_entrevues/exercises entrevues/exercises entrevues/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  exercises entrevues
+//
+//  Created by Pierre-Olivier Beaudoin on 2015-09-07.
+//  Copyright (c) 2015 Pierre-Olivier Beaudoin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
