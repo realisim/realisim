4d5c700830227d2e489a45b46a23981bb0685a45
Pierre-Olivier Beaudoin <pierreolivier.beaudoin@gmail.com>
2016-03-17 20:36:24 -0400

    Ajout de utils/statistics

---
+++ prototypes/statistics/main.cpp
@@ -0,0 +1,63 @@
+
+#include "utils/Statistics.h"
+#include "math/MathUtils.h"
+
+using namespace realisim;
+using namespace math;
+
+void print(const realisim::utils::Statistics& iSt)
+{
+  printf("number of sample %d\n"
+         "min %.4f\n"
+         "max %.4f\n"
+         "mean %.4f\n"
+         "std dev %.4f\n\n",
+         iSt.getNumberOfSamples(),
+         iSt.getMin(),
+         iSt.getMax(),
+         iSt.getMean(),
+         iSt.getStandardDeviation() );
+}
+
+bool test()
+{
+  const int NUM_SAMPLES = 10;
+  const double samples[] = {
+    6.1061334, 9.6783204, 1.2747090, 8.2395131, 0.3333483,
+    6.9755066, 1.0626275, 7.6587523, 4.9382973, 9.5788115
+  };
+
+  const double expect_min = 0.3333;
+  const double expect_max = 9.6783;
+  const double expect_mean = 5.584602;
+  const double expect_stddev = 3.547868;
+  
+  realisim::utils::Statistics st;
+  st.add(samples, NUM_SAMPLES);
+  print(st);
+  
+  const double epsilon = 0.0001;
+  return st.getNumberOfSamples() == NUM_SAMPLES &&
+    isEqual( st.getMin(), expect_min, epsilon) &&
+    isEqual( st.getMax(), expect_max, epsilon) &&
+    isEqual( st.getMean(), expect_mean, epsilon) &&
+  isEqual( st.getStandardDeviation(), expect_stddev, epsilon);
+  
+}
+
+int main(int argc, char** argv)
+{
+	
+  realisim::utils::Statistics st;
+  
+  for(int i = 0; i <= 200; ++i)
+  {
+    st.add(i);
+  }
+  
+  print(st);
+  
+  if( test() ) { printf("success"); }
+  else{ printf("failure"); }
+  return 0;
+}
---
+++ reusables/utils/Statistics.h
@@ -0,0 +1,61 @@
+
+#ifndef realisim_utils_statistics_hh
+#define realisim_utils_statistics_hh
+
+#include <vector>
+
+namespace realisim
+{
+namespace utils
+{
+
+  //Ce bout de code est fortement inspiré de :
+  //http://c.learncodethehardway.org/book/ex43.html
+  
+  //Cette classe sert a compiler des statistiques sur un flux d'échantillons.
+  //La classe permet optionellement de conserver les échantillons, sinon,
+  //les statisques sont calculés à la demande en temps constant O(1).
+  //
+  //On peut obtenir,
+  // min
+  // max
+  // mean
+  // nombre d'échantillons
+  // écart type
+  // 
+  class Statistics
+  {
+  public:
+    Statistics();
+    virtual ~Statistics();
+    
+    void add(double);
+    void add(const std::vector<double>&);
+    void add(const double*, unsigned int);
+    double getMax() const {return mMax;}
+    double getMean() const;
+    double getMin() const {return mMin;}
+    unsigned int getNumberOfSamples() const {return mNumberOfSamples;}
+    double getSample(unsigned int) const;
+    double getStandardDeviation() const;
+    bool isKeepingSamples() const {return mKeepSamples;}
+    void keepSamples(bool iK) {mKeepSamples = iK;}
+    
+  protected:
+    Statistics(const Statistics&);
+    Statistics& operator=(const Statistics&);
+    
+    void clear();
+    
+    bool mKeepSamples;
+    std::vector<double> mSamples;
+    unsigned int mNumberOfSamples;
+    double mSum;
+    double mSumSquared;
+    double mMin;
+    double mMax;
+  };
+
+} // end of namespace utils
+} // end of namespace realisim
+#endif
